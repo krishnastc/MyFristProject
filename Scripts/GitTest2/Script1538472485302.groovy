8bb03db09365dde64b47ddb2cdc0a0171c9b89f3
Shageerthana <Shageerthana@DESKTOP-PTEFVO1>
2018-10-02 14:59:11 +0530

    Test cases

--- Scripts/GitTest2/Script1538472485302.groovy
+++ Scripts/GitTest2/Script1538472485302.groovy
@@ -13,8 +13,7 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-'https://github.com/krishnastc'
-WebUI.openBrowser('')
+WebUI.openBrowser('http://www.shilpe.com/')
 
 WebUI.closeBrowser()
 
